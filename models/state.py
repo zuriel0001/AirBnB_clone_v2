f25836307ddf78543d647d4a7a9648cd82d45478
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-09-19 15:26:09 +0000

    city updated

--- models/state.py
+++ models/state.py
@@ -1,8 +1,50 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""The state class"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
+import models
+from models.city import City
+import shlex
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """
+    Class representing a state in the system.
+
+    Attributes:
+        __tablename__ (str): The name of the database table for states.
+        name (str): The name of the state.
+        cities (list): A list of City objects associated with this state.
+
+    Methods:
+        cities (property): Get a list of cities associated with this state.
+
+    Relationships:
+        cities (relationship): A relationship to the City model with cascade deletion.
+    """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
+
+    @property
+    def cities(self):
+        all_states = models.storage.all()
+        cities_list = []
+        matching_cities = []
+
+        for key in all_states:
+            city_data = key.replace('.', ' ')
+            city_data = shlex.split(city_data)
+
+            if city_data[0] == 'City':
+                cities_list.append(all_states[key])
+
+        for matching_city in cities_list:
+            if matching_city.state_id == self.id:
+                matching_cities.append(matching_city)
+
+        return matching_cities
