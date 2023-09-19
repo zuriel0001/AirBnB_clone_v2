2f4d98f12f1bf829af788cb2d861bc3c8c65a0a9
Chigalab1 <Chigozirimuwaezuoke@gmail.com>
2023-09-19 20:06:50 +0000

    fix error

--- models/user.py
+++ models/user.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
-from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
