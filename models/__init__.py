83661ed412fb81aef9836e4aaa38868966a354f9
Chigalab1 <Chigozirimuwaezuoke@gmail.com>
2023-09-23 23:05:51 +0000

    python

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""ate unique FileStorage instance for the application"""
+"""te unique FileStorage instance for the application"""
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
 from models.base_model import BaseModel
