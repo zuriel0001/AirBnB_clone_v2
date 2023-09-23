f23cab3cc9fe544c106c717519d958407fafd809
Chigalab1 <Chigozirimuwaezuoke@gmail.com>
2023-09-23 23:16:28 +0000

    pull

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""creates uniq FileStorage instance for the application"""
+"""creates uniqu FileStorage instance for the application"""
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
 from models.base_model import BaseModel
