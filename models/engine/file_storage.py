50b7b77891b70dff76d86e64c8349d695fb20099
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-09-23 13:50:13 +0000

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,70 +1,120 @@
 #!/usr/bin/python3
-"""Defines the FileStorage class."""
+"""This is the file storage class for AirBnB_v2"""
+
 import json
 from models.base_model import BaseModel
-from models.amenity import Amenity
+from models.user import User
+from models.state import State
 from models.city import City
+from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-from models.state import State
-from models.user import User
+import shlex
 
 
 class FileStorage:
-    """Represent an abstracted storage engine.
+    """
+    A class for managing storage and retrieval of objects to/from a JSON file.
 
     Attributes:
-        __file_path (str): The name of the file to save objects to.
-        __objects (dict): A dictionary of instantiated objects.
+        __file_path(str): path to the JSON file
+        __objects(dic): A dictionary to store objects with their unique keys.
     """
-
     __file_path = "file.json"
     __objects = {}
 
     def all(self, cls=None):
-        """Return a dictionary of instantiated objects in __objects.
-
-        If a cls is specified, returns a dictionary of objects of that type.
-        Otherwise, returns the __objects dictionary.
         """
-        if cls is not None:
-            if type(cls) == str:
-                cls = eval(cls)
-            cls_dict = {}
-            for k, v in self.__objects.items():
-                if type(v) == cls:
-                    cls_dict[k] = v
-            return cls_dict
-        return self.__objects
+        Retrieve all objects from the internal storage dictionary.
+
+        Args:
+           cls (Optional[Type]): If provided, filter objects by the specified class.
+
+        Returns:
+           dict: A dictionary containing objects from the storage dictionary.
+              If 'cls' is provided, only objects of the specified class are returned.
+        """
+        dic = {}
+        if cls:
+            dictionary = self.__objects
+            for key in dictionary:
+                partition = key.replace('.', ' ')
+                partition = shlex.split(partition)
+                if (partition[0] == cls.__name__):
+                    dic[key] = self.__objects[key]
+            return (dic)
+        else:
+            return self.__objects
 
     def new(self, obj):
-        """Set in __objects obj with key <obj_class_name>.id."""
-        self.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
+        """
+        Add a new object to the internal storage dictionary.
+
+        Args:
+          obj: The object to be added.
+
+        Returns:
+           None
+        """
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            self.__objects[key] = obj
 
     def save(self):
-        """Serialize __objects to the JSON file __file_path."""
-        odict = {o: self.__objects[o].to_dict() for o in self.__objects.keys()}
-        with open(self.__file_path, "w", encoding="utf-8") as f:
-            json.dump(odict, f)
+        """
+         Serialize objects from the internal storage dictionary to a JSON file.
+
+        The method iterates through the objects in `self.__objects`, converts them
+        to dictionaries using the `to_dict` method (assuming such a method exists
+        for the objects), and saves them to the JSON file specified by `self.__file_path`.
+
+    Returns:
+        None
+        """
+        my_dict = {}
+        for key, value in self.__objects.items():
+            my_dict[key] = value.to_dict()
+        with open(self.__file_path, 'w', encoding="UTF-8") as f:
+            json.dump(my_dict, f)
 
     def reload(self):
-        """Deserialize the JSON file __file_path to __objects, if it exists."""
+        """
+        Load and reload objects from a JSON file into the internal storage dictionary.
+
+        If the file specified by `self.__file_path` exists, it reads the JSON data,
+        reconstructs objects, and populates the internal dictionary with them.
+
+        If the file is not found, the method does nothing.
+
+        Returns:
+            None
+        """
         try:
-            with open(self.__file_path, "r", encoding="utf-8") as f:
-                for o in json.load(f).values():
-                    name = o["__class__"]
-                    del o["__class__"]
-                    self.new(eval(name)(**o))
+            with open(self.__file_path, 'r', encoding="UTF-8") as f:
+                for key, value in (json.load(f)).items():
+                    value = eval(value["__class__"])(**value)
+                    self.__objects[key] = value
         except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
-        """Delete a given object from __objects, if it exists."""
-        try:
-            del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
-        except (AttributeError, KeyError):
-            pass
+        """
+        Delete an object from the internal storage dictionary, if it exists.
+
+        Args:
+            obj: The object to be deleted. If provided, the method will attempt
+             to remove the object from the storage dictionary.
+
+        Return:
+            None
+        """
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            del self.__objects[key]
 
     def close(self):
-        """Call the reload method."""
+        """
+        Close and finalize any operations or resources related to this class.
+        """
         self.reload()
+
