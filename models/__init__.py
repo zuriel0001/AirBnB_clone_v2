f4714c4eb442d5d97cde8eb99672be9054b51041
Chigalab1 <Chigozirimuwaezuoke@gmail.com>
2023-09-23 23:11:28 +0000

    my commit

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""cre FileStorage instance for the application"""
+"""crea FileStorage instance for the application"""
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
 from models.base_model import BaseModel
