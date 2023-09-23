48c2222e0ffea4f940fd649362d2d4edff371e47
zuriel0001 <125594301+zuriel0001@users.noreply.github.com>
2023-09-23 16:34:13 +0000

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -13,6 +13,7 @@ from os import getenv
 
 
 if getenv("HBNB_TYPE_STORAGE") == "db":
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
     storage = FileStorage()
