6da05cd3d0a5c01fdd6b6810d9a2487e408e0f30
zuriel0001 <125594301+zuriel0001@users.noreply.github.com>
2023-09-23 16:49:33 +0000

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This is the state class"""
+"""Model for the state class"""
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
@@ -10,9 +10,9 @@ import shlex
 
 
 class State(BaseModel, Base):
-    """This is the class for State
+    """Represents the class for State
     Attributes:
-        name: input name
+        name: the input name
     """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
