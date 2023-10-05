39855995d8fe5e15b81ba7e336a3ffe010aa6522
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-10-05 16:26:28 +0000

    fixed output error

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -25,7 +25,7 @@ printf %s "server {
     }
 
     location /redirect_me {
-        return 301 http://cuberule.com/;
+        return 301 http://github.com/zuriel0001;
     }
 
     error_page 404 /404.html;
