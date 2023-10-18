e7978e7695571e268f0994d4ecc4d9851473ed78
zuriel0001 <125594301+zuriel0001@users.noreply.github.com>
2023-10-18 22:21:41 +0000

    Update 101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -14,7 +14,7 @@ $nginx_conf = "server {
     }
 
     location /redirect_me {
-        return 301 http://cuberule.com/;
+        return 301 https://github.com/;
     }
 
     error_page 404 /404.html;
