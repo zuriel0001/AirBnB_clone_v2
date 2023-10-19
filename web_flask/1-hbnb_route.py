a87402648664a1fbc40826c37ccd4821dc08b1bd
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-10-19 23:32:25 +0000

    Hbnb

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -19,14 +19,14 @@ def hello_hbnb():
     """
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Displays 'HBNB'."""
     return "HBNB"
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
-
 # if __name__ == "__main__":
-#    app.run(host="0.0.0.0", port=5000)
+#    app.run(host="0.0.0.0")
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
